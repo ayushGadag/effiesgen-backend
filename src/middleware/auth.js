c286663402b78199e1aeb9e51fefce3265a81210
Ayush <ayushgadag09@gmail.com>
2025-09-04 13:47:42 +0530

    done suthenctiation part

---
+++ src/middleware/auth.js
@@ -0,0 +1,20 @@
+// src/middleware/auth.js
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1]; // "Bearer token"
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // user info (id, role)
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+module.exports = auth;
