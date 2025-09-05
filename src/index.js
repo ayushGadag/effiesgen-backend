b562e627e72191ff62d0a99328b6794a6eb8eb7b
Ayush <ayushgadag09@gmail.com>
2025-09-05 13:15:28 +0530

    changes in index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const sequelize = require('./config/db');   // DB import
+
 const authRoutes = require('./routes/auth');
-const examRoutes = require('./routes/exam'); //new one//
+const examRoutes = require('./routes/exam');
+const settingsRoutes = require('./routes/settings');
+const collaborationRoutes = require('./routes/collaboration');
 
 const app = express();   
 app.use(express.json());
 
 // routes
 app.use('/api/auth', authRoutes);
-app.use('/api/exams', examRoutes);  // 
+app.use('/api/exams', examRoutes);
+app.use('/api/settings', settingsRoutes);
+app.use('/api/collaboration', collaborationRoutes);
 
 // test route
 app.get('/', (req, res) => {
