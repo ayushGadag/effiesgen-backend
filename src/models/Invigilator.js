c2fcba089a1f598146b8fa1891b57435f19a4f63
Ayush <ayushgadag09@gmail.com>
2025-09-05 13:24:25 +0530

    Invigilator.js added

---
+++ src/models/Invigilator.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Invigilator = sequelize.define('Invigilator', {
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  department: { type: DataTypes.STRING, allowNull: false }
+});
+
+module.exports = Invigilator;
